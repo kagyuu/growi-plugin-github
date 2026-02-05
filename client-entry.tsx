c062194a799ab54c195508f2026388e2669b6763
Atsushi <atsushi.hondoh@gmail.com>
2026-02-06 01:45:36 +0900

    Rename plugin to 'growi-plugin-amazon-embed' and implement Amazon embedding functionality

--- client-entry.tsx
+++ client-entry.tsx
@@ -1,5 +1,5 @@
 import config from './package.json';
-import { plugin } from './src/today';
+import { plugin } from './src/amazon';
 import { Options, Func, ViewOptions } from './types/utils';
 
 declare const growiFacade : {
