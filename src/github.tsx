90ff4a59c9f07868e4071d10501fd6ac51086763
Atsushi <atsushi.hondoh@gmail.com>
2026-02-11 21:53:17 +0900

    Refactor code structure for improved readability and maintainability

--- src/github.tsx
+++ src/github.tsx
@@ -57,9 +57,9 @@ const createCode = async function(attributeLang: string, attributeUrl: string): 
 
     const html = [];
     html.push('<div class="growi-plugin-github-window-title">');
-    html.push(`<a href="${attributeUrl}" target="_blank" rel="noopener noreferrer">${attributeUrl}</a>`);
+    html.push(`<a href="${attributeUrl}" target="_blank" rel="noopener noreferrer">🏷${attributeUrl}</a>`);
     html.push('</div>');
-    html.push('<div class="growi-plugin-github-code"><pre><code>');
+    html.push(`<div class="growi-plugin-github-code"><pre><code class="language-${attributeLang}">\n`);
     html.push(hljs.highlight(content, { language: attributeLang, ignoreIllegals: true}).value);
     html.push('</code></pre></div>');
     return html.join('');
